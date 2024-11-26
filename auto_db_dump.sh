94a664e7d43f86fe7992e5038f41f039baee98db
DuttonMa <duttonma@gmail.com>
2024-11-26 14:10:19 +0000

    Update script

--- auto_db_dump.sh
+++ auto_db_dump.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-platform db:dump -p 6tlkpwbr6tndk -e main -r uregni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r odscni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r fiscalcommissionni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r mahinquiry -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r nifiscalcouncil -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r octf -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r niauditoffice -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r employmenttribunalsni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r independentreviewofeducation -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r nipolicingboard -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r pbni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r investnireview -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r attorneygeneralni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r cscsreviewni -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r pcsps -y
-platform db:dump -p 6tlkpwbr6tndk -e main -r urologyservicesinquiry -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r afbi -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r niaomi -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r nipb -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r pbni -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r pps -y
+platform db:dump -p 6gl4nbwlja4u4 -e main -r uregni -y
+
 
