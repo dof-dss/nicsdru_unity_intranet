f33d1b1bbac153d32f6584a0bfb4f6e9dd279271
DuttonMa <duttonma@gmail.com>
2025-08-18 09:51:38 +0100

    Run test as if on main branch

--- web/sites/site.settings.php
+++ web/sites/site.settings.php
@@ -84,7 +84,7 @@ if (!empty(getenv('PLATFORM_BRANCH'))) {
 
   // Environment specific settings and services.
   switch (getenv('PLATFORM_BRANCH')) {
-    case 'main':
+    case 'pbni_solr_config_testing':
       // De-facto production settings.
       $settings['container_yamls'][] = $app_root . '/' . $site_path . '/../services.yml';
       // Use 'production' config split for the Platform.sh production site.
